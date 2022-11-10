f89e62e4546cec77990e178e3a8622a801d9d136
Maurin <114497410+maurin7@users.noreply.github.com>
2022-11-10 20:28:56 +0100

    Cleaned up create react app

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+  <nav style={{display: `flex`, justifyContent: `flex-end`}}> 
+  
+<p>Sign Out</p>
+
+  </nav>
+  )
+}
+
+
+export default Navigation
